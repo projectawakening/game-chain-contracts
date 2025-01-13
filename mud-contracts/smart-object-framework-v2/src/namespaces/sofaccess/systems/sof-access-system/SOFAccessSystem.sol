a142a6af017ee4939acae6057a225355158cca78
ccp-tezza <118321367+ccp-tezza@users.noreply.github.com>
2025-01-13 19:47:40 +0800

    improvement: update the sof-v2 to use uint256 IDs for entities (#363)
    A new PR for the SOF V2 to use `uint256` IDs for entities instead of the
    proposed Id format. This change will make integration with the existing
    infrastructure much easier.
    
    New Tag structure -
    
    To accommodate this change the Tag structure has undergone an update as
    well. There are now 3 types of tags: PROPERTY, ENTITY RELATION, and
    RESOURCE RELATION tags.
    **Property Tags** - these are tags to add some property to an entity,
    the SOF uses these to track the `CLASS` and `OBJECT` properties of an
    Entity.
    **Entity Relation Tags** - these tags are used to track some
    relationship between Entities. The SOF uses these tags to track the
    INHERITANCE relationship between OBJECTs and CLASSes
    **Resource Relation Tags**- these tag can be used to track some
    relationship between an Entity and a MUD Resource. The SOF uses these
    tags to track which MUD Systems are associated via COMPOSITION to an
    Entity for the purpose of enforcing functionality scope.
    
    All Tags can now have a value assigned to them when they are tagged to
    an Entity.
    
    More details can be found
    [here](https://github.com/projectawakening/world-chain-contracts/blob/ccp-tezza/sof-v2-entity-tag-update/mud-contracts/smart-object-framework-v2/README.md)
    .

--- mud-contracts/smart-object-framework-v2/src/namespaces/sofaccess/systems/sof-access-system/SOFAccessSystem.sol
+++ mud-contracts/smart-object-framework-v2/src/namespaces/sofaccess/systems/sof-access-system/SOFAccessSystem.sol
@@ -5,15 +5,15 @@ import { ResourceId, WorldResourceIdInstance } from "@latticexyz/world/src/World
 import { NamespaceOwner } from "@latticexyz/world/src/codegen/tables/NamespaceOwner.sol";
 import { SystemRegistry } from "@latticexyz/world/src/codegen/tables/SystemRegistry.sol";
 
-import { Objects } from "../../../evefrontier/codegen/tables/Objects.sol";
-import { Classes } from "../../../evefrontier/codegen/tables/Classes.sol";
-import { ClassSystemTagMap } from "../../../evefrontier/codegen/tables/ClassSystemTagMap.sol";
-import { ObjectSystemTagMap } from "../../../evefrontier/codegen/tables/ObjectSystemTagMap.sol";
+import { Entity } from "../../../evefrontier/codegen/tables/Entity.sol";
+import { EntityTagMap } from "../../../evefrontier/codegen/tables/EntityTagMap.sol";
+
 import { Role } from "../../../evefrontier/codegen/tables/Role.sol";
 import { HasRole } from "../../../evefrontier/codegen/tables/HasRole.sol";
 
-import { Id } from "../../../../libs/Id.sol";
-import { ENTITY_CLASS, ENTITY_OBJECT } from "../../../../types/entityTypes.sol";
+import { TagId, TagIdLib } from "../../../../libs/TagId.sol";
+
+import { TAG_TYPE_PROPERTY, TAG_TYPE_ENTITY_RELATION, TAG_TYPE_RESOURCE_RELATION, TAG_IDENTIFIER_CLASS, TagParams, EntityRelationValue, ResourceRelationValue } from "../../../evefrontier/systems/tag-system/types.sol";
 
 import { ISOFAccessSystem } from "../../interfaces/ISOFAccessSystem.sol";
 import { IWorldWithContext } from "../../../../IWorldWithContext.sol";
@@ -37,9 +37,9 @@ contract SOFAccessSystem is ISOFAccessSystem, SmartObjectFramework {
    * @param targetCallData The calldata of the target function
    * @dev Reverts if caller doesn't have the required role
    */
-  function allowClassAccessRole(Id classId, bytes memory targetCallData) public view {
-    if (!HasRole.get(Classes.getAccessRole(classId), _callMsgSender(1))) {
-      revert SOFAccess_RoleAccessDenied(Classes.getAccessRole(classId), _callMsgSender(1));
+  function allowClassAccessRole(uint256 classId, bytes memory targetCallData) public view {
+    if (!HasRole.get(Entity.getAccessRole(classId), _callMsgSender(1))) {
+      revert SOFAccess_RoleAccessDenied(Entity.getAccessRole(classId), _callMsgSender(1));
     }
   }
 
@@ -49,26 +49,36 @@ contract SOFAccessSystem is ISOFAccessSystem, SmartObjectFramework {
    * @param targetCallData The calldata of the target function
    * @dev Handles both direct calls (call depth 1) and class system-scoped calls (call depth > 1)
    */
-  function allowClassScopedSystemOrDirectClassAccessRole(Id entityId, bytes memory targetCallData) public view {
+  function allowClassScopedSystemOrDirectClassAccessRole(uint256 entityId, bytes memory targetCallData) public view {
     uint256 callCount = IWorldWithContext(_world()).getWorldCallCount();
-    Id classId;
-    if (entityId.getType() == ENTITY_CLASS) {
+    uint256 classId;
+    if (EntityTagMap.getHasTag(entityId, TagIdLib.encode(TAG_TYPE_PROPERTY, TAG_IDENTIFIER_CLASS))) {
       classId = entityId;
     } else {
-      classId = Objects.getClass(entityId);
+      EntityRelationValue memory entityRelationValue = abi.decode(
+        EntityTagMap.getValue(entityId, TagIdLib.encode(TAG_TYPE_ENTITY_RELATION, bytes30(bytes32(entityId)))),
+        (EntityRelationValue)
+      );
+      classId = entityRelationValue.relatedEntityId;
     }
+
     // a direct entrypoint call to EntitySystem.sol will put this access call at callCount = 1
     if (callCount > 1) {
       // not a direct entrypoint call to EntitySystem.sol but allowable if the previous call was from a Class scoped System
       (, , address msgSender, ) = IWorldWithContext(_world()).getWorldCallContext();
       ResourceId callingSystemId = SystemRegistry.get(msgSender);
-      if (!ClassSystemTagMap.getHasTag(classId, Id.wrap(ResourceId.unwrap(callingSystemId)))) {
+      if (
+        !EntityTagMap.getHasTag(
+          classId,
+          TagIdLib.encode(TAG_TYPE_RESOURCE_RELATION, bytes30(ResourceId.unwrap(callingSystemId)))
+        )
+      ) {
         revert SOFAccess_SystemAccessDenied(classId, msgSender);
       }
     } else if (callCount == 1) {
-      // if this is direct call to EntitySystem.sol, we check for Class access role membership
-      if (!HasRole.get(Classes.getAccessRole(classId), _callMsgSender(1))) {
-        revert SOFAccess_RoleAccessDenied(Classes.getAccessRole(classId), _callMsgSender(1));
+      // if this is direct call to EntitySystem.sol, we check for access role membership
+      if (!HasRole.get(Entity.getAccessRole(classId), _callMsgSender(1))) {
+        revert SOFAccess_RoleAccessDenied(Entity.getAccessRole(classId), _callMsgSender(1));
       }
     } else {
       // callCount = 0, means this access function was called directly. It should only be called via an access() modifier from another function, so we revert
@@ -81,21 +91,29 @@ contract SOFAccessSystem is ISOFAccessSystem, SmartObjectFramework {
    * @param targetCallData The calldata of the target function
    * @dev Handles both direct calls (call depth 1) and class system-scoped calls (call depth > 1)
    */
-  function allowClassScopedSystemOrDirectObjectAccessRole(Id objectId, bytes memory targetCallData) public view {
+  function allowClassScopedSystemOrDirectObjectAccessRole(uint256 objectId, bytes memory targetCallData) public view {
     uint256 callCount = IWorldWithContext(_world()).getWorldCallCount();
     // a direct entrypoint call to EntitySystem.sol will put this access call at callCount = 1
     if (callCount > 1) {
       // not a direct entrypoint call to EntitySystem.sol but allowable if the call was from an Class scoped System
       (, , address msgSender, ) = IWorldWithContext(_world()).getWorldCallContext();
       ResourceId callingSystemId = SystemRegistry.get(msgSender);
-      Id classId = Objects.getClass(objectId);
-      if (!ClassSystemTagMap.getHasTag(classId, Id.wrap(ResourceId.unwrap(callingSystemId)))) {
+      EntityRelationValue memory entityRelationValue = abi.decode(
+        EntityTagMap.getValue(objectId, TagIdLib.encode(TAG_TYPE_ENTITY_RELATION, bytes30(bytes32(objectId)))),
+        (EntityRelationValue)
+      );
+      if (
+        !EntityTagMap.getHasTag(
+          entityRelationValue.relatedEntityId,
+          TagIdLib.encode(TAG_TYPE_RESOURCE_RELATION, bytes30(ResourceId.unwrap(callingSystemId)))
+        )
+      ) {
         revert SOFAccess_SystemAccessDenied(objectId, msgSender);
       }
     } else if (callCount == 1) {
-      // if this is direct call to EntitySystem.sol, we check for Object access role membership
-      if (!HasRole.get(Objects.getAccessRole(objectId), _callMsgSender(1))) {
-        revert SOFAccess_RoleAccessDenied(Objects.getAccessRole(objectId), _callMsgSender(1));
+      // if this is direct call to EntitySystem.sol, we check for access role membership
+      if (!HasRole.get(Entity.getAccessRole(objectId), _callMsgSender(1))) {
+        revert SOFAccess_RoleAccessDenied(Entity.getAccessRole(objectId), _callMsgSender(1));
       }
     } else {
       // callCount = 0, means this access function was called directly. It should only be called via an access() modifier from another function, so we revert
@@ -110,30 +128,20 @@ contract SOFAccessSystem is ISOFAccessSystem, SmartObjectFramework {
    * @dev Handles access control for TagSystem.sol functionality (only callable from EntitySystem.sol or directly via a role member)
    * @dev TODO: Add HookSystem.sol access to this when implemented
    */
-  function allowEntitySystemOrDirectAccessRole(Id entityId, bytes memory targetCallData) public view {
+  function allowEntitySystemOrDirectAccessRole(uint256 entityId, bytes memory targetCallData) public view {
     uint256 callCount = IWorldWithContext(_world()).getWorldCallCount();
     // a direct entrypoint call to TagSystem.sol will put this access call at callCount = 1
     if (callCount > 1) {
       // if not a direct entrypoint call to TagSystem, (instead a subsequent internal call), then we only allow EntitySystem.sol as the target System of this function
-      (ResourceId systemId, , address msgSender, ) = IWorldWithContext(_world()).getWorldCallContext();
-
-      if (systemId.unwrap() != EntitySystemUtils.entitySystemId().unwrap()) {
+      (, , address msgSender, ) = IWorldWithContext(_world()).getWorldCallContext();
+      ResourceId callingSystemId = SystemRegistry.get(msgSender);
+      if (callingSystemId.unwrap() != EntitySystemUtils.entitySystemId().unwrap()) {
         // for TagSystem if an internal call is not from EntitySytem or a Class scoped system then we reject the call
         revert SOFAccess_SystemAccessDenied(entityId, msgSender);
       }
     } else if (callCount == 1) {
-      // this is direct call to TagSystem.sol
-      bytes2 entityType = entityId.getType();
-      if (entityType == ENTITY_CLASS) {
-        // if the entity is a Class, we check for Class access role membership
-        if (!HasRole.get(Classes.getAccessRole(entityId), _callMsgSender(1))) {
-          revert SOFAccess_RoleAccessDenied(Classes.getAccessRole(entityId), _callMsgSender(1));
-        }
-      } else {
-        // if the entity is an Object, we check for Object access role membership
-        if (!HasRole.get(Objects.getAccessRole(entityId), _callMsgSender(1))) {
-          revert SOFAccess_RoleAccessDenied(Objects.getAccessRole(entityId), _callMsgSender(1));
-        }
+      if (!HasRole.get(Entity.getAccessRole(entityId), _callMsgSender(1))) {
+        revert SOFAccess_RoleAccessDenied(Entity.getAccessRole(entityId), _callMsgSender(1));
       }
     } else {
       // callCount = 0, means this access function was called directly. It should only be called via an access() modifier, so we revert
