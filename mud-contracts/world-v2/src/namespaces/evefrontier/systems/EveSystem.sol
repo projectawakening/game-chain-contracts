deb6744a745b545f9a9b1b4439b368b62a3e156a
CCP Legolas <toniya@ccpgames.com>
2025-02-28 22:13:39 +0000

    lint

--- mud-contracts/world-v2/src/namespaces/evefrontier/systems/EveSystem.sol
+++ mud-contracts/world-v2/src/namespaces/evefrontier/systems/EveSystem.sol
@@ -47,7 +47,7 @@ import { IEveSystem } from "../interfaces/IEveSystem.sol";
  * @notice This is the base system to be inherited by all other systems.
  * @dev Consider combining this with the SmartObjectSystem which is extended by all systems.
  */
-contract EveSystem is SmartObjectFramework, IEveSystem {
+contract EveSystem is IEveSystem, SmartObjectFramework {
   /**
    * @notice Get the world instance
    * @return The IWorld instance
